88d47e9348b0ae0a49e547cb03b7499e57218377
SoltidueMarquez <2630388173@qq.com>
2024-12-14 00:55:44 +0800

    八度音阶

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,21 +1,45 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// 地图生成器
-/// </summary>
 public class MapGenerator : MonoBehaviour {
 
 	public int mapWidth;
 	public int mapHeight;
 	public float noiseScale;
 
+	[Tooltip("八度音阶的数量")] public int octaves;
+	[Range(0, 1), Tooltip("持久性值")] public float persistance;
+	[Tooltip("间隙度值")] public float lacunarity;
+
+	[Tooltip("种子")] public int seed;//如果 seed 是固定的，则每次运行程序时，生成的随机数序列都是一样的
+	[Tooltip("向量偏移量")] public Vector2 offset;
+
 	public bool autoUpdate;
 
 	public void GenerateMap() {
-		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, noiseScale);
-		
+		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+
 		MapDisplay display = FindObjectOfType<MapDisplay> ();
 		display.DrawNoiseMap (noiseMap);
 	}
-	
+
+	//使用验证方法控制值，该方法会在脚本变量值在inspector中改变时自动调用
+	void OnValidate() {
+		//地图宽高始终大于等于1
+		if (mapWidth < 1) {
+			mapWidth = 1;
+		}
+		if (mapHeight < 1) {
+			mapHeight = 1;
+		}
+		//间隙度始终大于等于1
+		if (lacunarity < 1) {
+			lacunarity = 1;
+		}
+		//八度音调层数始终大于等于0
+		if (octaves < 0) {
+			octaves = 0;
+		}
+	}
+
 }
--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -2,22 +2,81 @@
 
 public static class Noise {
 
-	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale) {
-		var noiseMap = new float[mapWidth,mapHeight];//声明一个地图大小的噪音数组
+	/// <summary>
+	/// 噪音生成函数，使用柏林噪音与八度音阶叠加
+	/// </summary>
+	/// <param name="mapWidth">地图宽度</param>
+	/// <param name="mapHeight">地图高度</param>
+	/// <param name="seed">种子用于生成相同的地图</param>
+	/// <param name="scale">缩放比例，用于避免整数</param>
+	/// <param name="octaves">八度音阶的数量</param>
+	/// <param name="persistance">持久性值</param>
+	/// <param name="lacunarity">间隙度值</param>
+	/// <param name="offset">向量偏移</param>
+	/// <returns></returns>
+	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
+		float[,] noiseMap = new float[mapWidth,mapHeight];
+		
+		//八度音阶采样偏移
+		System.Random prng = new System.Random (seed);//伪随机数生成器
+		//八度音阶偏移向量数组
+		Vector2[] octaveOffsets = new Vector2[octaves];
+		//循环生成每个八度的偏移量，对于每个八度生成一个随机数（范围在 -100000 到 100000 之间），并与 offset.x 和 offset.y 进行相加
+		for (int i = 0; i < octaves; i++) {
+			float offsetX = prng.Next (-100000, 100000) + offset.x;
+			float offsetY = prng.Next (-100000, 100000) + offset.y;
+			octaveOffsets [i] = new Vector2 (offsetX, offsetY);//这些偏移量存储在 octaveOffsets 数组中
+		}
+		
 		//判断比例大小避免除零错误
 		if (scale <= 0) {
 			scale = 0.0001f;
 		}
+		
+		//记录最大和最小的噪声值
+		float maxNoiseHeight = float.MinValue;
+		float minNoiseHeight = float.MaxValue;
+
+		float halfWidth = mapWidth / 2f;
+		float halfHeight = mapHeight / 2f;
+
 		//设置噪声地图
 		for (int y = 0; y < mapHeight; y++) {
 			for (int x = 0; x < mapWidth; x++) {
-				var sampleX = x / scale;
-				var sampleY = y / scale;
-				//调用柏林函数的数学库
-				var perlinValue = Mathf.PerlinNoise (sampleX, sampleY);
-				noiseMap [x, y] = perlinValue;
+		
+				float amplitude = 1;	//设置振幅
+				float frequency = 1;	//设置频率，越高采样点之间的间隙越远，高度值变化更加迅速
+				float noiseHeight = 0;	//当前高度
+				
+				//使用循环来遍历所有的八度音阶
+				for (int i = 0; i < octaves; i++) {
+					//通过乘以频率来调整采样坐标，每个八度音阶从不同的地方采样，同时做数学处理保证改变scale时从中心改变
+					float sampleX = (x-halfWidth) / scale * frequency + octaveOffsets[i].x;
+					float sampleY = (y-halfHeight) / scale * frequency + octaveOffsets[i].y;
+					//调用柏林函数的数学库叠加八度音阶，将其范围改到[-1,1]
+					float perlinValue = Mathf.PerlinNoise (sampleX, sampleY) * 2 - 1;
+					noiseHeight += perlinValue * amplitude;
+					//更新八度音阶
+					amplitude *= persistance;
+					frequency *= lacunarity;
+				}
+				//更新最大和最小的噪声值
+				if (noiseHeight > maxNoiseHeight) {
+					maxNoiseHeight = noiseHeight;
+				} else if (noiseHeight < minNoiseHeight) {
+					minNoiseHeight = noiseHeight;
+				}
+				noiseMap [x, y] = noiseHeight;//应用噪声高度
 			}
 		}
+		
+		//反向插值使得噪声值归一化
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				noiseMap [x, y] = Mathf.InverseLerp (minNoiseHeight, maxNoiseHeight, noiseMap [x, y]);
+			}
+		}
+
 		return noiseMap;
 	}
 
