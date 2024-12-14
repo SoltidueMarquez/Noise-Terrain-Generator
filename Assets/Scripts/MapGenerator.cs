c819adfe93a336e45599b889356b6099504a8b15
SoltidueMarquez <2630388173@qq.com>
2024-12-14 23:53:02 +0800

    颜色

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,7 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
+[Serializable]
+public enum DrawMode {NoiseMap, ColourMap};
 public class MapGenerator : MonoBehaviour {
-
+	[Tooltip("绘制模式")] public DrawMode drawMode;
+	
+	[Header("地图设置")]
 	public int mapWidth;
 	public int mapHeight;
 	public float noiseScale;
@@ -13,14 +18,33 @@ public class MapGenerator : MonoBehaviour {
 	[Tooltip("种子")] public int seed;//如果 seed 是固定的，则每次运行程序时，生成的随机数序列都是一样的
 	[Tooltip("向量偏移量")] public Vector2 offset;
 
-	public bool autoUpdate;
+	[Tooltip("是否自动更新")] public bool autoUpdate;
 
+	[Tooltip("地形类型数组")] public TerrainType[] regions;
+	
 	public void GenerateMap() {
 		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+		Color[] colourMap = new Color[mapWidth * mapHeight];//声明一维颜色地图
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				float currentHeight = noiseMap [x, y];//获取当前地图点高度
+				for (int i = 0; i < regions.Length; i++) {//遍历所有地形类型设置当前点的类型
+					if (currentHeight <= regions [i].height) {//如果当前区域小于等于地形类型规定的高度就设置颜色地图
+						colourMap [y * mapWidth + x] = regions [i].colour;
+						break;
+					}
+				}
+			}
+		}
 
+		//可视化展示
 		MapDisplay display = FindObjectOfType<MapDisplay> ();
-		display.DrawNoiseMap (noiseMap);
+		if (drawMode == DrawMode.NoiseMap) {
+			display.DrawTexture (TextureGenerator.TextureFromHeightMap(noiseMap));
+		} else if (drawMode == DrawMode.ColourMap) {
+			display.DrawTexture (TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+		}
 	}
 
 	//使用验证方法控制值，该方法会在脚本变量值在inspector中改变时自动调用
@@ -41,5 +65,14 @@ public class MapGenerator : MonoBehaviour {
 			octaves = 0;
 		}
 	}
+}
 
+/// <summary>
+/// 地形类型结构体
+/// </summary>
+[System.Serializable]
+public struct TerrainType {
+	[Tooltip("地形名称")] public string name;
+	[Tooltip("地形高度")] public float height;
+	[Tooltip("对应颜色")] public Color colour;
 }
---
+++ Assets/Scripts/TextureGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 纹理生成器
+/// </summary>
+public static class TextureGenerator {
+	// 绘制颜色地图返回贴图
+	public static Texture2D TextureFromColourMap(Color[] colourMap, int width, int height) {
+		Texture2D texture = new Texture2D (width, height);
+		texture.filterMode = FilterMode.Point;//纹理过滤模式设置为点模式，防止模糊
+		texture.wrapMode = TextureWrapMode.Clamp;//纹理的包裹模式设置为钳制模式防止纹理堆叠
+		texture.SetPixels (colourMap);
+		texture.Apply ();
+		return texture;
+	}
+
+	// 绘制噪声地图方法
+	public static Texture2D TextureFromHeightMap(float[,] heightMap) {
+		int width = heightMap.GetLength (0);
+		int height = heightMap.GetLength (1);
+		//使用黑白颜色插值来绘制纹理像素
+		Color[] colourMap = new Color[width * height];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				colourMap [y * width + x] = Color.Lerp (Color.black, Color.white, heightMap [x, y]);
+			}
+		}
+
+		return TextureFromColourMap (colourMap, width, height);
+	}
+
+}
